6f115c962e339e05c6c7d6eebb8f17f554eb1e70
RamMohan112 <51237043+RamMohan112@users.noreply.github.com>
2020-07-31 15:36:48 +0530

    Add files via upload

---
+++ piu_run_demo.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DATASET=cifar100  #cub flower imagenet_sub
+GPU=0
+NET=resnet18 # resnet32
+LR=1e-6 
+EPOCH=51 
+SAVE=50
+LOSS=triplet_no_hard_mining #triplet
+TASK=11 #6
+BASE=50 #17
+SEED=1
+
+for Method in Finetuning #LwF EWC MAS
+do
+for Tradeoff in 0 # 1 1e7 1e6
+do
+
+NAME=${Method}_${Tradeoff}_${DATASET}_${LOSS}_${NET}_${LR}_${EPOCH}epochs_task${TASK}_base${BASE}_seed${SEED}
+
+python train.py -base ${BASE} -seed ${SEED} -task ${TASK} -data ${DATASET} -tradeoff ${Tradeoff} -exp ${Tradeoff} -net ${NET} -method ${Method} \
+-lr ${LR} -dim 512  -num_instances 8 -BatchSize 32 -loss ${LOSS}  -epochs ${EPOCH} -log_dir ${DATASET}_seed${SEED}/${NAME}  \
+-save_step ${SAVE} -gpu ${GPU} 
+
+done
+done
