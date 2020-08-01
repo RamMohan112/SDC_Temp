887ff4fb5f1a0cca6a87ffa63850ee4acdc7ef94
RamMohan112 <51237043+RamMohan112@users.noreply.github.com>
2020-08-01 14:11:08 +0530

    Update piu_run_demo.sh

--- piu_run_demo.sh
+++ piu_run_demo.sh
@@ -20,23 +20,23 @@ echo $Method
 
 NAME=${Method}_${Tradeoff}_${DATASET}_${LOSS}_${NET}_${LR}_${EPOCH}epochs_task${TASK}_base${BASE}_seed${SEED}
 
-# python train.py -base ${BASE} -seed ${SEED} -task ${TASK} -data ${DATASET} -tradeoff ${Tradeoff} -exp ${Tradeoff} -net ${NET} -method ${Method} \
-# -lr ${LR} -dim 512  -num_instances 8 -BatchSize 32 -loss ${LOSS}  -epochs ${EPOCH} -log_dir ${DATASET}_seed${SEED}/${NAME}  \
-# -save_step ${SAVE} -gpu ${GPU} 
+python train.py -base ${BASE} -seed ${SEED} -task ${TASK} -data ${DATASET} -tradeoff ${Tradeoff} -exp ${Tradeoff} -net ${NET} -method ${Method} \
+-lr ${LR} -dim 512  -num_instances 8 -BatchSize 32 -loss ${LOSS}  -epochs ${EPOCH} -log_dir ${DATASET}_seed${SEED}/${NAME}  \
+-save_step ${SAVE} -gpu ${GPU} 
 
 
-# python test.py  -seed ${SEED} -base ${BASE} -task ${TASK} -epochs ${EPOCH} -data ${DATASET} -gpu ${GPU} -method ${Method} -r  \
-# checkpoints/${DATASET}_seed${SEED}/${NAME} >./results/${DATASET}/${NAME}_old_mean.txt 
+python test.py  -seed ${SEED} -base ${BASE} -task ${TASK} -epochs ${EPOCH} -data ${DATASET} -gpu ${GPU} -method ${Method} -r  \
+checkpoints/${DATASET}_seed${SEED}/${NAME} >./results/${DATASET}/${NAME}_old_mean.txt 
 
 
-# for SIGMA_TEST in 0.20 #0.30 
-# do
+for SIGMA_TEST in 0.20 #0.30 
+do
 
-# python test.py -seed ${SEED} -base ${BASE} -task ${TASK} -epochs ${EPOCH} -data ${DATASET} -gpu ${GPU} -method ${Method} -r  \
-# checkpoints/${DATASET}_seed${SEED}/${NAME} -mapping_test -sigma_test ${SIGMA_TEST} \
-# >./results/${DATASET}/${NAME}_SDC_sigma_test${SIGMA_TEST}.txt 
+python test.py -seed ${SEED} -base ${BASE} -task ${TASK} -epochs ${EPOCH} -data ${DATASET} -gpu ${GPU} -method ${Method} -r  \
+checkpoints/${DATASET}_seed${SEED}/${NAME} -mapping_test -sigma_test ${SIGMA_TEST} \
+>./results/${DATASET}/${NAME}_SDC_sigma_test${SIGMA_TEST}.txt 
 
-# done
+done
 
 done
 done
